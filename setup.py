3364bf4aa94534d36366850c90cd2a566f805d54
riyaz <riyazj82@gmail.com>
2024-04-21 17:30:10 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Riyaz',
+    author_email= 'riyazj82@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
+
